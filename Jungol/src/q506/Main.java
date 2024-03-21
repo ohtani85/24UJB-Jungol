edf9b8fb07a7a93f43cedbe05cb272d5c18cc404
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 15:48:39 +0900

    Q/A

--- Jungol/src/q506/Main.java
+++ Jungol/src/q506/Main.java
@@ -3,7 +3,7 @@ package q506;
 public class Main {
 	public static void main(String[] args) {
 		int var1 = 170;
-		double var2 = 68.600000;
+		double var2 = 68.6;
 		
 		System.out.println("My height");
 		System.out.println(var1);
