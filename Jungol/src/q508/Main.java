5033f85623561834b58c199860f50535014729ab
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 15:55:46 +0900

    수정완료

--- Jungol/src/q508/Main.java
+++ Jungol/src/q508/Main.java
@@ -2,10 +2,9 @@ package q508;
 
 public class Main {
 	public static void main(String[] args) {
-
-		        System.out.printf("%10s%10s%10s\n", "item", "count", "price");
-		        System.out.printf("%10s%10d%10d\n", "pen", 20, 100);
-		        System.out.printf("%10s%10d%10d\n", "note", 5, 95);
-		        System.out.printf("%10s%10d%10d\n", "eraser", 110, 97);
-		    }
+		System.out.printf("%10s%10s%10s\n", "item", "count", "price");
+		System.out.printf("%10s%10d%10d\n", "pen", 20, 100);
+		System.out.printf("%10s%10d%10d\n", "note", 5, 95);
+		System.out.printf("%10s%10d%10d\n", "eraser", 110, 97);
 		}
+	}
