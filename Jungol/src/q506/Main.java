6f6315bf5ca06ad4567e5c9a7a410a27f36abec8
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-21 12:40:46 +0900

    8th Commit

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,13 @@
+package q506;
+
+public class Main {
+	public static void main(String[] args) {
+		int var1 = 170;
+		double var2 = 68.600000;
+		
+		System.out.println("My height");
+		System.out.println(var1);
+		System.out.println("My weight");
+		System.out.printf("%.6f",var2);
+	}
+}
