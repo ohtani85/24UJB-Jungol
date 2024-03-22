11d5158d3c8d7c30931a2a78bc09880cd0330f1e
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-22 10:06:54 +0900

    qq

--- Jungol/src/q515/Main.java
+++ Jungol/src/q515/Main.java
@@ -8,13 +8,14 @@ public class Main {
 
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-
+        sc.close();      
+        
         // 곱셈 결과
         int value1 = num1 * num2;
         // 몫 계산
         int value2 = num1 / num2;
 
-        // 결과 출력
+        // 결과 출력14
         System.out.println(num1 + " * " + num2 + " = " + value1);
         System.out.println(num1 + " / " + num2 + " = " + value2);
 		
