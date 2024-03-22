d4a4202cdb42246b9844b9a818ffe5981e29d9a0
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-22 10:27:25 +0900

    q106

---
+++ Jungol/src/q106/Main.java
@@ -0,0 +1,12 @@
+package q106;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        System.out.println(num1 + " + " + num2 + " = " + num3);
+    }
+}
