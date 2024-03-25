6270c54c26e40cfac16085d13f2c694e228eec25
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-25 17:30:58 +0900

    q9039

---
+++ Jungol/src/q9039/Main.java
@@ -0,0 +1,9 @@
+package q9039;
+
+public class Main {
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch); // 공백 없이 문자 출력
+        }
+    }
+}
