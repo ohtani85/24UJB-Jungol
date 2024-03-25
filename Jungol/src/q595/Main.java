24ef9d4626397458c8eabf51254a0fa7f2e4dd8b
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-25 14:29:36 +0900

    q595

---
+++ Jungol/src/q595/Main.java
@@ -0,0 +1,10 @@
+package q595;
+
+public class Main {
+	public static void main(String[] args) {
+		String str = "Hong Gil Dong";
+		for (int i = 3; i <= 6; i++) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
