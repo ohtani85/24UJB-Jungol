2d955918f23ab3e38a971bb22ec2b502e7881f78
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-27 10:20:02 +0900

    q533 수정

--- Jungol/src/q533/Main.java
+++ Jungol/src/q533/Main.java
@@ -9,18 +9,18 @@ public class Main {
 		String gender = sc.next(); // 성별 입력 ('M' 또는 'F')
 		int age = sc.nextInt(); // 나이 입력
 
-		// 성별과 나이에 따라 조건 분기
+
 		if ("M".equals(gender)) {
 			if (age >= 18) {
-				System.out.println("MAN"); // 성인남자
+				System.out.println("MAN");
 			} else {
-				System.out.println("BOY"); // 미성년남자
+				System.out.println("BOY");
 			}
 		} else if ("F".equals(gender)) {
 			if (age >= 18) {
-				System.out.println("WOMAN"); // 성인여자
+				System.out.println("WOMAN");
 			} else {
-				System.out.println("GIRL"); // 미성년여자
+				System.out.println("GIRL");
 			}
 		} 
 
