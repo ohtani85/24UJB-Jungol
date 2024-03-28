11bbf6e8eccf75d77ce133162c00b9b648ca3200
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-28 17:05:07 +0900

    q107

---
+++ Jungol/src/q107/Main.java
@@ -0,0 +1,14 @@
+package q107;
+
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 80.5;
+        double num2 = 22.34;
+
+        double sum = num1 + num2;
+        
+        System.out.printf("%10.2f", num1);
+        System.out.printf("%10.2f", num2);
+        System.out.printf("%10.2f", sum);
+    }
+}
