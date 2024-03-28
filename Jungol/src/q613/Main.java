d895f2a52cd4d6004aeceee7074aec9f289782db
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-28 10:06:43 +0900

    q613

--- Jungol/src/q613/Main.java
+++ Jungol/src/q613/Main.java
@@ -4,38 +4,38 @@ import java.util.Scanner;
 
 //학생 정보를 저장할 클래스 정의
 class Student {
- String name;
- String school;
- int grade;
+	String name;
+	String school;
+	int grade;
 
- // 생성자
- public Student(String name, String school, int grade) {
-     this.name = name;
-     this.school = school;
-     this.grade = grade;
- }
+	// 생성자
+	public Student(String name, String school, int grade) {
+		this.name = name;
+		this.school = school;
+		this.grade = grade;
+	}
 
- // 학생 정보를 출력하는 메소드
- public void printInfo() {
-     System.out.println("Name : " + name);
-     System.out.println("School : " + school);
-     System.out.println("Grade : " + grade);
- }
+	// 학생 정보를 출력하는 메소드
+	public void printInfo() {
+		System.out.println("Name : " + name);
+		System.out.println("School : " + school);
+		System.out.println("Grade : " + grade);
+	}
 }
 
 public class Main {
- public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-     // 사용자로부터 입력 받기
-     String input = sc.nextLine();
-     String[] parts = input.split(" ");
+		// 사용자로부터 입력 받기
+		String input = sc.nextLine();
+		String[] parts = input.split(" ");
 
-     // 입력받은 정보를 바탕으로 Student 객체 생성
-     Student student = new Student(parts[0], parts[1], Integer.parseInt(parts[2]));
+		// 입력받은 정보를 바탕으로 Student 객체 생성
+		Student student = new Student(parts[0], parts[1], Integer.parseInt(parts[2]));
 
-     // 학생 정보 출력
-     student.printInfo();
-     sc.close();
- }
+		// 학생 정보 출력
+		student.printInfo();
+		sc.close();
+	}
 }
