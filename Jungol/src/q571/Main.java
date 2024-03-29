39e8345f56e7c6e067a24e6dc466284e142076c3
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-29 14:05:32 +0900

    q571

---
+++ Jungol/src/q571/Main.java
@@ -0,0 +1,17 @@
+package q571;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        for(int i = 0; i < number; i++) {
+            System.out.println("~!@#$^&*()_+|");
+        }
+
+        sc.close();
+    }
+}
