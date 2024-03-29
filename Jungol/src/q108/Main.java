b2ab0e8e398ae9a39da91c69c6abd6bd9f4d6b39
21-manic-008g <21-manic-008g@21-manic-008>
2024-03-29 09:30:34 +0900

    q108

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,12 @@
+package q108;
+
+public class Main {
+	public static void main(String[] args) {
+		int num1 = 50;
+		double num2 = 100.12;
+		double sum = num2 * num1;
+	
+		System.out.println(num2 + " * " + num1 + " = " + (int)sum);
+
+	}
+}
