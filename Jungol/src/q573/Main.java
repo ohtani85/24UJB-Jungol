0da50cc8169c90b0f1da0b663d6ee356a7c0b2d5
21-manic-008g <21-manic-008g@21-manic-008>
2024-04-05 10:23:14 +0900

    q573

--- Jungol/src/q573/Main.java
+++ Jungol/src/q573/Main.java
@@ -7,13 +7,16 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		Square(n);
 		sc.close();
+	}
 
-		int num = 1;
+	private static void Square(int n) {
+		int num = 1; 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				System.out.print(num + " ");
-				num++;
+				num++; 
 			}
 			System.out.println();
 		}
