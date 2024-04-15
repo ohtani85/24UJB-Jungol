28ae4bd759239c549dbf0add17134a3999ee785b
21-manic-008g <21-manic-008g@21-manic-008>
2024-04-15 10:01:06 +0900

    qq

--- Jungol/src/q535/Main.java
+++ Jungol/src/q535/Main.java
@@ -12,31 +12,30 @@ public class Main {
 			return;
 		}
 
-		// 분류 값을 정의하기 위한 변수
 		int category;
 
 		if (grade >= 4.0) {
-			category = 1;
-		} else if (grade >= 3.0) {
-			category = 2;
-		} else if (grade >= 2.0) {
-			category = 3;
-		} else {
 			category = 4;
+		} else if (grade >= 3.0) {
+			category = 3;
+		} else if (grade >= 2.0) {
+			category = 2;
+		} else {
+			category = 1;
 		}
 
-		// switch문을 사용하여 결과 출력
+
 		switch (category) {
-		case 1:
+		case 4:
 			System.out.println("scholarship");
 			break;
-		case 2:
+		case 3:
 			System.out.println("next semester");
 			break;
-		case 3:
+		case 2:
 			System.out.println("seasonal semester");
 			break;
-		case 4:
+		case 1:
 			System.out.println("retake");
 			break;
 		default:
