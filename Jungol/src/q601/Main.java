373433c98a53bde237d3dd23496b68468de9566c
21-manic-008g <21-manic-008g@21-manic-008>
2024-04-19 09:30:12 +0900

    qq

--- Jungol/src/q601/Main.java
+++ Jungol/src/q601/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("문자열을 입력하세요 (100자 이하):");
 		String input = sc.nextLine();
 
 		int length = input.length();
