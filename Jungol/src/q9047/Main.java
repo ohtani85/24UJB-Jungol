13bd490544adb40a364f483faffd327e98bb8479
21-manic-008g <21-manic-008g@21-manic-008>
2024-05-28 09:52:51 +0900

    커밋

---
+++ Jungol/src/q9047/Main.java
@@ -0,0 +1,14 @@
+package q9047;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  char c = 'A';
+	        
+	        while (c <= 'Z') {
+	            System.out.print(c);
+	            c++;
+	        }
+	    }
+	
+	}
